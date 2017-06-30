466f909423413f8886d69f97641d61f092c771ca
Kinice <szp93@126.com>
2017-06-30 11:17:02 +0800

    Version 1.1.0: Add search history,fix garbled problem.

--- popup.js
+++ popup.js
@@ -1,27 +1,35 @@
- //xhr object
-    var xhr = new XMLHttpRequest()
-    var url = 'http://kinice.top/api/qrimage'
-    //Elements
-    var textarea = document.getElementById('text'),
-        btn = document.getElementById('btn'),
-        qrcon = document.getElementById('qr')
-    //get function
-    var postRequest = function(text){
-        xhr.open('POST',url,true)
-        xhr.setRequestHeader("Content-Type","application/x-www-form-urlencoded");
-        xhr.send('text='+encodeURIComponent(text))
-    }
-    btn.onclick = function(e){
-        e.preventDefault()
-        xhr.onreadystatechange = function(){
-            if(xhr.readyState == 4){
-                if(xhr.status == 200){
-                    var data = JSON.parse(xhr.responseText)
-                    qrcon.innerHTML = data.qr_string
-                }else{
-                    console.log(xhr.statusText)
-                }
+//xhr object
+var xhr = new XMLHttpRequest()
+var url = 'http://kinice.top/api/qrimage'
+var qrurl = localStorage.qrurl || '';
+//Elements
+var textarea = document.getElementById('text'),
+    btn = document.getElementById('btn'),
+    qrcon = document.getElementById('qr')
+//get function
+var postRequest = function(text){
+    xhr.open('POST',url,true)
+    xhr.setRequestHeader("Content-Type","application/x-www-form-urlencoded")
+    xhr.send('text='+encodeURIComponent(text))
+}
+
+if(qrurl!==''){
+    textarea.value = qrurl
+}
+
+btn.onclick = function(e){
+    e.preventDefault()
+    if(textarea.value == '') return
+    localStorage.qrurl = textarea.value
+    xhr.onreadystatechange = function(){
+        if(xhr.readyState == 4){
+            if(xhr.status == 200){
+                var data = JSON.parse(xhr.responseText)
+                qrcon.innerHTML = data.qr_string
+            }else{
+                console.log(xhr.statusText)
             }
         }
-        postRequest(textarea.value)
     }
+    postRequest(textarea.value)
+}
