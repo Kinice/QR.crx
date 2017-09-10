f4328acc3c895c3f7a35c0e6846071eb7cb3f276
Kinice <szp93@126.com>
2017-09-11 00:46:28 +0800

    [feature] Add [Create this page's qr code] button.

--- popup.js
+++ popup.js
@@ -5,6 +5,7 @@ var qrurl = localStorage.qrurl || '';
 //Elements
 var textarea = document.getElementById('text'),
     btn = document.getElementById('btn'),
+    thisbtn = document.getElementById('this-btn'),
     qrcon = document.getElementById('qr')
 //get function
 var postRequest = function(text){
@@ -32,4 +33,25 @@ btn.onclick = function(e){
         }
     }
     postRequest(textarea.value)
+}
+
+thisbtn.onclick = function(e){
+    e.preventDefault()
+    
+    chrome.tabs.getSelected(null, function (tab) {
+        textarea.value = tab.url
+        if(textarea.value == '') return
+        localStorage.qrurl = textarea.value
+        xhr.onreadystatechange = function(){
+            if(xhr.readyState == 4){
+                if(xhr.status == 200){
+                    var data = JSON.parse(xhr.responseText)
+                    qrcon.innerHTML = data.qr_string
+                }else{
+                    console.log(xhr.statusText)
+                }
+            }
+        }
+        postRequest(textarea.value)
+    });
 }
